d87f7bd00cab523c63779ef8af2cae1b3cd9c8c0
Mertkan Kececi <47403024+mertkan-k@users.noreply.github.com>
2019-12-02 19:16:46 +0300

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,15 @@
+printf "Starting..\n"
+
+x=0
+y=0
+for q in $(find ./all_quests -name "*.lua" -type f); do
+    if ./qc $q >/dev/null 2>&1; then
+        # printf "Succeeded: $q\n"
+        x=$((x+1))
+    else
+        printf "Failed: $q\n"
+        y=$((y+1))
+    fi
+done
+
+printf "All quests done; succes: $((x)), fail $((y)).\n"
